26deb5cd8ccc6aeb689a0e6a3970a4205eda1f3f
Pietrzak Mariusz <web@pietrzakmariusz.dev>
2021-07-10 00:39:27 +0200

    fixed import filename

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import Layout from '../components/layout/layout';
+import Layout from '../components/layout/Layout';
 import { NotificationContextProvider } from '../store/NotificationContext';
 
 import '../styles/globals.css';
